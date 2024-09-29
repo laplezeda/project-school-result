35fb257365355e88e93be37b5610ca964ac75b6a
laplezeda <166850341+laplezeda@users.noreply.github.com>
2024-09-29 12:39:42 +0530

    Add files via upload

--- project file programs.py
+++ project file programs.py
@@ -11,25 +11,28 @@ elif login2 != "password" :
 else:
     print("Wrong Usename")
 
-
-#creates the database
-databasename = input("enter database name :")
-mycursor.execute("create database %s ;" %databasename)
-print("Database created") 
-"""
 while True :
     #takes input if the user is student or teacher
     a = input("Please Choose from the below \n a.) Teacher \n b.) Student \n enter choise here :")  
     if a == "a" :
-        print("1.) Create Table \n 2.) Enter Result \n 3.) Update result  \n 4.)View request for Rechecking ")
+        print("1.) Create Table \n 2.) Enter Result \n 3.) Update result  \n 4.)View request for Rechecking \n 5.) Create database  ")
         k = int(input("Enter choice :"))
         if k == 1:
 
             #makes tables(for teacher )
             table_name =  input("enter table name")
-            mycursor.execute("
+            mycursor.execute("create table %s( " %table_name)
 
         elif k == 2 :
+
+
+#creates the database
+databasename = input("enter database name :")
+mycursor.execute("create database %s ;" %databasename)
+mycursor.execute("use %s  ;" %databasename)
+print("Database created") 
+
+
             
     #shows the result of the concerned student
     #request rechecking (for student)
@@ -37,4 +40,3 @@ while True :
     #update grades (for teacher)
     #
 
-"""

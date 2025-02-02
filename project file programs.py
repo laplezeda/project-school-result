6bd64887120c85c8c71adb34a9e52fe5741be7cd
Deepanshu Chauhan <166850341+laplezeda@users.noreply.github.com>
2025-02-02 21:33:01 +0530

    Add files via upload

--- project file programs.py
+++ project file programs.py
@@ -1,84 +1,239 @@
-#connection
-import mysql.connector 
-login1 = input("Enter user ")
-login2 = input("enter  password")
+# Login and connection
+import mysql.connector
+print("-"*50) 
+print("LOGIN  :")
+login1 = input("Enter user : ")
+login2 = input("enter  password : ")
+print("-"*50)
+
 if login1 == "user" and login2 == "password":
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="admin")
-    mycursor = mydb.cursor
-elif login2 != "password" :
-    print("wrong password")
+    mycursor = mydb.cursor()
+
 else:
-    print("Wrong Usename")
+    print("Wrong Username or password")
 
-while True :
+while login1 == "user" and login2 == "password" :
+    
     #takes input if the user is student or teacher
-    a = input("Please Choose from the below \n a.) Teacher \n b.) Student \n enter choise here :")  
-    if a == "a" :
-        print("1.) Create database  \n 2.) Create table  \n 3.) Enter Result \n 4.) Update result  \n 5.)View request for Rechecking  ")
-        k = int(input("Enter choice :"))
-        if k == 2:
+    
+    print("-"*50)  
+    print("Please Choose from the below")
+    print("a.) Teacher")
+    print("b.) Student")
+    print("c.) Exit ")
+    print("-"*50)
 
-            #makes tables(for teacher )
+    a = input("Enter choice here :")
+    if a == "a" :
+        
+        print("-"*50)
+        print("1.) Create database")
+        print("2.) Create table ")
+        print("3.) Enter Result ")
+        print("4.) Update result")
+        print("5.) Delete a record in table")
+        print("6.) Delete a table ")
+        print("7.) View request for Rechecking ")
+        print("8.) Display structure of a table  ")
+        print("9.) Delete a database")
+        print("10.) Display no. of records in a table ")
+        print("11.) Display the tables in a database ")
+        print("12.) Exit ")
+        print("-"*50)
+
+        k = int(input("Enter choice :"))
+        
+        if k == 1 :
+            
+            #creates the database
+            
+            databasename = input("enter database name :")
+            mycursor.execute("create database %s ;" %databasename)
+            mycursor.execute("use %s  ;" %databasename)
+            print("Database created")
+            mydb.commit()
+        
+        elif k == 2:
+
+            #Creates custum tables accordings to the specific needs 
+            
+            s = input("Enter the database you wish to use ")
+            mycursor.execute("use "+ s)
             table_name=input("Enter Table Name:\t")
             column=input("Enter first Column:\t")
             datatype=input("Enter Datatype:\t")
             constraint=input("Enter Constraint:\t")
             mycursor.execute("CREATE TABLE " + table_name + "(" + column +" "+ datatype +" "+ constraint + ")")
+            
             while True:
-                a = int(input("do you wish to add more columns y/n"))
+                a = input("do you wish to add more columns y/n")
+                
                 if a == "y":
-                    column=input("Enter first Column:\t")
+                    column=input("Enter  Column Name:\t")
                     datatype=input("Enter Datatype:\t")
                     constraint=input("Enter Constraint:\t")
-                    mycursor.execute("alter table " + table_name + "add " + column +" "+ datatype +" "+ constraint)
+                    mycursor.execute("alter table " + table_name + " add " + column +" "+ datatype +" "+ constraint)
                 elif a=="n":
                     break
             mydb.commit()
 
-        elif k == 1 :
-            #creates the database
-            databasename = input("enter database name :")
-            mycursor.execute("create database %s ;" %databasename)
-            mycursor.execute("use %s  ;" %databasename)
-            print("Database created")
         elif k == 3:
+            
             #enter the result (for teacher )
-            g = input("enter table name you wish to enter record in : ")
+
+            s = input("Enter the database you wish to use ")
+            g = input("Enter table name you wish to enter record in : ")
+            
+            while True :
+                e = input("Enter values seperated by comma : ")
+                mycursor.execute("use "+ s)
+                mycursor.execute("insert into " + g + " values(" + e + " )")
+                mydb.commit()
+                j = input("do you wish to add more records y/n")
+                if j == "n" :
+                    break
 
 
         elif k == 4:
-            #update grades (for teacher)
-            c = input("enter class of student ")
-            h = input("enter rollno of student: ")
-            f = input("enter what to update ")
-            d = input("enter new value ")
-            mycursor.execute("update table class"+c+ " set "+f +" = "+d+ " where rollno = "+h )
+            #update records (for teacher)
+            s = input("Enter the database you wish to use ")
+            c = input("Enter class of student ")
+            h = input("Enter rollno of student: ")
+            f = input("Enter what to update ")
+            d = input("Enter new value ")
+            
+            mycursor.execute("use "+ s)
+            mycursor.execute("update class_"+c+ " set "+f +" = "+d+ " where roll_no = "+h )
             mydb.commit()
 
 
-        elif k== 5 :
-            mycursor.execute("slelect * form rechecking ")
+        elif k == 5 :
+
+            #To delete a record
+
+            s = input("Enter the database you wish to use ")
+            c = input("Enter class of student ")
+            h = input("Enter rollno of student: ")
+            
+            mycursor.execute("use " + s)
+            mycursor.execute("delete from class_"+ c + " where roll_no = "+h ) 
+            mydb.commit()
+
+        elif k == 6 :
+
+            #To delete a Table 
+
+            s = input("Enter the database you wish to use ")
+            c = input("Enter the name of table ")
+
+            mycursor.execute("use " + s)
+            mycursor.execute("drop table "+ c)
+            mydb.commit()
+
+        
+        elif k== 7 :
+            
+            # to view rechecking table 
+            mycursor.execute("use rechecking")
+            mycursor.execute("Select * from rechecking ")
             rec = mycursor.fetchall()
             for i in rec :
                 print(i)
+        
 
+        elif k == 8 :
+            
+            # to view the structure of a table 
+            
+            s = input("Enter the database you wish to use ")
+            q = input("Enter the name of the table :  ")
+            mycursor.execute("use "+ s)
+            mycursor.execute("describe "+ q)            
+            rec = mycursor.fetchall() 
+            for i in rec:
+                print(i)
+        
+        elif k == 9 :
 
+            #To delete a database
 
+            s = input("Enter the database you wish to Delete  ")
+            mycursor.execute("Drop database " + s)
+            mydb.commit()     
+
+        elif k == 10 : 
+            
+            # To count the no of records in a table 
+
+            s = input("Enter the database you wish to use ")
+            c = input("Enter Name of table ")
+            
+            mycursor.execute("use "+ s)
+            mycursor.execute("select count(*) from " + c)
+            rec = mycursor.fetchone()
+            print(rec[0])
+
+        elif k == 11 : 
+
+            # To display the tables in a database
+
+            s = input("Enter the database you wish to use ")
+
+            mycursor.execute("use "+ s)
+            mycursor.execute("show tables ")
+            rec = mycursor.fetchall()
+            for i in rec:
+                print(i)
+                
+        elif k == 12 : 
+            break
+
+        else :
+            print("Enter a valid choice ")
+    
     elif a == "b":
-        student_choice =int(input("Please Choose from the below \n 1.) show result  \n 2.) request rechecking \n enter choise here : "))
+        print("-"*50)
+        print("Please Choose from the below")
+        print("1.) Show result ")
+        print("2.) Request rechecking ")
+        print("4.) Exit")
+        print("-"*50)
+        student_choice =int(input("Enter choice here : "))
+        
+        
         if student_choice == 1 :
+            
             #shows the result of the concerned student
-            enter_rollno = int(input("Enter your roll no. : "))
+            mycursor.execute("use student_marks")
+            enter_rollno = input("Enter your roll no. : ")
             enter_class = input("Enter your class : ")
-            mycursor.execute("select * from class"+enter_class + " where roll_no ="+enter_rollno)
+            mycursor.execute("select * from class_"+enter_class +" where roll_no = "+enter_rollno)
             rec = mycursor.fetchall()
             for i in rec :
                 print(i)
+        
+        
         elif student_choice == 2 :
-             #request rechecking (for student)
-            j = input("enter rollno : ")
-            k = input ("enter subject : ")
-            mycursor.execute("insert into rechecking values("+j +","+k+" )")
+            
+            #request rechecking (for student)
+            
+            j = input("Enter rollno and subject seprated by comma : ")
+            mycursor.execute("use rechecking")
+            mycursor.execute("insert into rechecking values( "+ j+" )")
+            mydb.commit()
+        
+        elif student_choice == 3 :
+            break   
+        
+        else :
+            print("Enter a valid choice ")
+    
+    elif a == "c" :
+        break
+    
+    else :
+        print("Enter a valid choice ")
